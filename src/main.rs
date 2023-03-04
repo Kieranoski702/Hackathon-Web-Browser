ba429313c331a0aad8a658319b87d5b5baf05d1e
Kieranoski <kfowlds1812@gmail.com>
2023-03-04 13:33:23 +0000

    Added command line interface parsing using the clap library. By default will open index.html and pass the character stream to parser or if using the search subcommand will request that url and send the html file returned as a character stream to the parser

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 mod html_adt;
 use std::fs::File;
 use std::io::{BufReader, Read};
-mod Parser;
+use clap::{Parser, Subcommand};
+mod HtmlParser;
 mod Renderer;
+mod Requester;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Search for the given url on the internet
+    Search { url: Option<String> },
+}
 
 fn main() {
-    // Read the contents of the index.html file into a string
-    let mut file = File::open("index.html").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents);
+    let cli = Cli::parse();
+    let contents: String;
+    match &cli.command {
+        Some(Commands::Search { url }) => {
+            let url = url.clone().unwrap();
+            contents = Requester::request(&url).unwrap();
+        }
+        None => {
+            // Read the contents of the index.html file into a string
+            let mut file = File::open("index.html").unwrap();
+            file.read_to_string(&mut contents).unwrap();
+        }
+    }
 
-    // Pass the contents of the file to the parser
-    let parsed_html = Parser::parse(&contents);
-
-    // Pass the parsed HTML to the renderer
+    let parsed_html = HtmlParser::parsehtml(&contents);
     Renderer::render(parsed_html);
 }

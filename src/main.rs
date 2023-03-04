1c2e2da16ef068dbcd0f9a0c6f0910f4b21a0f82
Kieranoski <kfowlds1812@gmail.com>
2023-03-04 12:02:06 +0000

    Created skeleton main and very simple html index file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+mod Parser;
+mod Renderer;
+
 fn main() {
-    println!("Hello, world!");
+    // Read the contents of the index.html file into a string
+    let mut file = File::open("index.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+
+    // Pass the contents of the file to the parser
+    let parsed_html = Parser::parse(&contents);
+
+    // Pass the parsed HTML to the renderer
+    Renderer::render(parsed_html);
 }
